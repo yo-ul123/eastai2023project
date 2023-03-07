7479ffba80f541f111d40643cf582807a8dd381c
BuckbeakThePizza <alice.n.karakachian@gmail.com>
2023-03-06 21:09:07 -0500

    Moved player code into a Player class and changed jumps to be charged when the screen is held.
    TODO: add maximum x/y jump charge.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,82 +1,32 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
 	Texture arrow;
-
-	private Sprite sprite;
+	private Player player;
 	private Sprite right;
-
-	float x = 0;
-	float y = 0;
-	float ySpeed = 0;
-	float xSpeed = 0;
-	float gravity = -1;
-	float friction = -1;
-	float maxSpeed = 20;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("frog.png");
+		player = new Player();
 		arrow = new Texture("rightArrow.png");
-		sprite = new Sprite(img, 0, 0, 64, 64);
 		right = new Sprite(arrow, 0, 0, 32, 32);
 		right.setPosition(500, 10);
 	}
 
 	@Override
 	public void render () {
-		if(Gdx.input.justTouched())
-		{
-			if(y == 0) {
-				ySpeed += 10;
-			}
-		}
-		if(Gdx.input.isTouched())
-		{
-			Rectangle bounds = right.getBoundingRectangle();
-			if(bounds.contains(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY())) //y coordinate is flipped for getY()
-			{
-				xSpeed += 2;
-			}
-		}
-		if(y > 0)
-		{
-			ySpeed += gravity;
-		}
-		if(xSpeed > maxSpeed)
-		{
-			xSpeed = maxSpeed;
-		}
-		if(xSpeed > 0)
-		{
-			xSpeed += friction;
-		}
-		if(xSpeed < 0)
-		{
-			xSpeed -= friction;
-		}
-		y += ySpeed;
-		x += xSpeed;
-		if(y<0) {
-			y = 0;
-			ySpeed = 0;
-		}
-		sprite.setRotation(ySpeed);
-		sprite.setPosition(x, y);
+		player.update(right);
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		sprite.draw(batch);
+		player.getSprite().draw(batch);
 		right.draw(batch);
 		batch.end();
 	}
@@ -84,6 +34,6 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		player.dispose();
 	}
 }
---
+++ core/src/com/mygdx/game/Player.java
@@ -0,0 +1,78 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Player {
+    Sprite frog;
+    Texture img;
+    float chargeX;
+    float chargeY;
+    boolean jump;
+    float x = 0;
+    float y = 0;
+    float ySpeed = 0;
+    float xSpeed = 0;
+    float gravity = -1;
+    float friction = -1;
+    float maxSpeed = 20;
+
+    public Player() {
+        img = new Texture("frog.png");
+        frog = new Sprite(img, 0, 0, 64, 64);
+    }
+
+    public Sprite getSprite() {
+        return frog;
+    }
+
+    public void update(Sprite right) {
+        if (jump) {
+            ySpeed += chargeY;
+            xSpeed += chargeX;
+            chargeX = 0;
+            chargeY = 0;
+            jump = false;
+        }
+        if (Gdx.input.isTouched()) {
+            if(y == 0) {
+                chargeY += 1;
+                Rectangle bounds = right.getBoundingRectangle();
+                if (bounds.contains(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY())) //y coordinate is flipped for getY()
+                {
+                    chargeX += 1;
+                }
+            }
+        }
+        if (!Gdx.input.isTouched())
+        {
+            jump = true;
+        }
+        if (y > 0) {
+            ySpeed += gravity;
+        }
+        if (xSpeed > maxSpeed) {
+            xSpeed = maxSpeed;
+        }
+        if (xSpeed > 0) {
+            xSpeed += friction;
+        }
+        if (xSpeed < 0) {
+            xSpeed -= friction;
+        }
+        y += ySpeed;
+        x += xSpeed;
+        if (y < 0) {
+            y = 0;
+            ySpeed = 0;
+        }
+        frog.setRotation(ySpeed);
+        frog.setPosition(x, y);
+    }
+    public void dispose()
+    {
+        img.dispose();
+    }
+}
